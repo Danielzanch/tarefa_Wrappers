04c62fb9ebcbe89222f525689b381769f3691be6
Daniel Zanchetta <danibrurib@gmail.com>
2024-10-07 20:41:53 -0300

    carregando projeto no remote

---
+++ src/App.java
@@ -0,0 +1,39 @@
+public class App {
+    public static void main(String[] args) {
+        int num1 = 10;
+        Integer num2 = num1;
+        System.out.println();
+        System.out.println("*** int para Integer ***");
+        System.out.println(num1);
+        System.out.println(num2);
+
+        double d1 = 5.2;
+        Double double1 = d1;
+        System.out.println();
+        System.out.println("*** double para float ***");
+        System.out.println(d1);
+        System.out.println(double1);
+
+        long l1 = 245874;
+        Long long1 = l1;
+        System.out.println();
+        System.out.println("*** long para Long ***");
+        System.out.println(l1);
+        System.out.println(long1); 
+
+        boolean b1 = false;
+        Boolean boolean1 = b1;
+        System.out.println();
+        System.out.println("*** boolean para Boolean ***");
+        System.out.println(b1);
+        System.out.println(boolean1);
+
+        char c1 = 'B';
+        Character character = c1;
+        System.out.println();
+        System.out.println("*** char para Character ***");
+        System.out.println(c1);
+        System.out.println(character);
+
+    }
+}
